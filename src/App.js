f045dffaeec655c5355df835b328af7faa8130e6
Mason Hirst <mason.j.hirst@gmail.com>
2023-01-04 11:31:22 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css'
+import Header from './Components/Header'
+import MainDisplay from './Components/MainDisplay'
+import OptionDisplay from './Components/OptionDisplay'
+import { selectPotentials } from './redux/slices/potentialCountriesSlice'
+import { useSelector } from 'react-redux'
+
+function App() {
+   return (
+      <div className="App font-link">
+         <Header />
+         <OptionDisplay />
+      </div>
+   )
+}
+
+export default App
